255af398327ef13ed21a956e97622445d8ab619c
TheLecus <marcosgf2005@gmail.com>
2022-09-23 21:13:18 +0200

    Added alerts

--- dist/astra.js
+++ dist/astra.js
@@ -4,3 +4,15 @@ function copyToClipboard(text){
 function redirect(href){
     window.location.href = href;
 }
+
+Array.from(document.getElementsByClassName("close")).forEach(button =>  {
+    button.addEventListener("click", () => {
+        setInterval(function(){
+            button.parentElement.style.opacity = parseFloat(getComputedStyle(button.parentElement).opacity - 0.1)
+            if(getComputedStyle(button.parentElement).opacity <= 0){
+                button.parentElement.remove();
+            }
+        },50);
+    });
+});
+
