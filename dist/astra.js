d12d1a73783243362ef98f93b2475793257309f6
TheLecus <marcosgf2005@gmail.com>
2022-09-24 18:03:24 +0200

    Added input general validation

--- dist/astra.js
+++ dist/astra.js
@@ -15,4 +15,40 @@ Array.from(document.getElementsByClassName("close")).forEach(button =>  {
         },50);
     });
 });
+Array.from(document.getElementsByClassName("validation")).forEach(e =>  {
+    e.addEventListener("input", () => {
+        let isValid = false;
+        switch(e.getAttribute("type")){
+            case "mail":
+                isValid = e.value.includes("@") && e.value.includes(".") ? true: false;
+                break;
+            case "checkbox":
+                if(e.hasAttribute("required")){
+                    isValid = e.checked;
+                }
+                else{
+                    isValid = true;
+                }
+                break;
+            default:
+                isValid = e.value != "" ? true: false;
+                break;
+        }
+        if(isValid){
+            e.classList.add("valid");
+            e.classList.remove("invalid");
+            
+        }
+        else if(e.value != ""){
+            e.classList.remove("valid");
+            e.classList.add("invalid");
+        }
+        else{
+            e.classList.remove("valid");
+            e.classList.remove("invalid");
+        }
+    }); 
+});
+
+
 
