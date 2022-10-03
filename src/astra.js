d2f03c3387429591033d4cf84d57a3b9ec80af91
TheLecus <marcosgf2005@gmail.com>
2022-10-03 16:56:15 +0200

    Improved function class filtering system

--- src/astra.js
+++ src/astra.js
@@ -1,23 +1,23 @@
 const copyToClipboard = text => {
     navigator.clipboard.writeText(text);
-}
+};
 const redirect = href => {
     window.location.href = href;
-}
+};
 const getPosition = element => {
     return new Vector2(element.getBoundingClientRect().x,element.getBoundingClientRect().y);
-}
+};
 class Vector2{
     constructor(x,y){
         this.x = x;
         this.y = y;
     }
-}
+};
 //Close button
 Array.from(document.getElementsByClassName("close")).forEach(button =>  {
     button.addEventListener("click", () => {
         setInterval(function(){
-            button.parentElement.style.opacity = parseFloat(getComputedStyle(button.parentElement).opacity - 0.1)
+            button.parentElement.style.opacity = parseFloat(getComputedStyle(button.parentElement).opacity - 0.1);
             if(getComputedStyle(button.parentElement).opacity <= 0){
                 button.parentElement.remove();
             }
@@ -48,7 +48,6 @@ Array.from(document.getElementsByClassName("validation")).forEach(e =>  {
         if(isValid){
             e.classList.add("valid");
             e.classList.remove("invalid");
-            
         }
         else if(e.value != ""){
             e.classList.remove("valid");
@@ -63,13 +62,13 @@ Array.from(document.getElementsByClassName("validation")).forEach(e =>  {
 
 //Gradient function
 const Gradient = (mode,color1,color2,rotation = "") => {
-    if(mode == "radial"){ rotation = "" }
-    if(color1.toString().startsWith("--")){ color1 = `var(${color1})` }
-    if(color2.toString().startsWith("--")){ color2 = `var(${color2})` }
-    return(`${(mode == "linear")? "linear-gradient(" : "radial-gradient("}${(rotation == "")? "": rotation+","}${color1},${color2})`)
+    if(mode == "radial"){ rotation = ""; }
+    if(color1.toString().startsWith("--")){ color1 = `var(${color1})`; }
+    if(color2.toString().startsWith("--")){ color2 = `var(${color2})`; }
+    return(`${(mode == "linear")? "linear-gradient(" : "radial-gradient("}${(rotation == "")? "": rotation+","}${color1},${color2})`);
 };
 const BackgroundColor = (color) => {
-    return(`${(color.startsWith("--"))? "var(" + color + ")": color}`)
+    return(`${(color.startsWith("--"))? "var(" + color + ")": color}`);
 }
 const parseFunctionClass = (element,name) => {
     element = element.replace(name,"");
@@ -83,19 +82,34 @@ Array.from(document.querySelectorAll("*")).forEach(e => {
     if(c != null){
         c = c.toString();
         c.split(" ").forEach(groupElement => {
-            if(groupElement.startsWith("gradient")){
-                groupElement = parseFunctionClass(groupElement,"gradient");
-                let attributes = groupElement.split(",");
-                e.style.background = Gradient(attributes[0],attributes[1],attributes[2],attributes[3]);
-            }
-            else if(groupElement.startsWith("bg")){
-                groupElement = parseFunctionClass(groupElement,"bg");
-                e.style.backgroundColor = BackgroundColor(groupElement);
-            }
-            else if(groupElement.startsWith("span")){
-                groupElement = parseFunctionClass(groupElement,"span");
-                e.style.flexGrow = groupElement;
-            }
+            if(groupElement.substring(0, groupElement.indexOf("("))){
+                switch(groupElement.substring(0, groupElement.indexOf("("))){
+                    case "gradient":
+                        groupElement = parseFunctionClass(groupElement,"gradient");
+                        let attributes = groupElement.split(",");
+                        e.style.background = Gradient(attributes[0],attributes[1],attributes[2],attributes[3]);
+                        break;
+                    case "bg":
+                        groupElement = parseFunctionClass(groupElement,"bg");
+                        e.style.backgroundColor = BackgroundColor(groupElement);
+                        break;
+                    case "span":
+                        groupElement = parseFunctionClass(groupElement,"span");
+                        e.style.flexGrow = groupElement;
+                        break;
+                    case "width":
+                        groupElement = parseFunctionClass(groupElement,"width");
+                        e.style.width = groupElement;
+                        break;
+                    case "heigth":
+                        groupElement = parseFunctionClass(groupElement,"height");
+                        e.style.height = groupElement;
+                        break;
+                    default:
+                        console.error(`The "·${groupElement.substring(0, groupElement.indexOf("("))}" function class doesn't exist`);
+                        break;
+                }
+            } 
         });
     }
 });
