42774effa998367da2ceec72b6607f22b1723ef5
ImLecus <marcosgf2005@gmail.com>
2022-11-11 19:57:42 +0100

    Added base.css

--- astra/astra.js
+++ astra/astra.js
@@ -1,23 +1,25 @@
-var fs = require("node:fs")
+console.log("Compiling...");
+var startTime = performance.now()
+var fs = require("node:fs");
+
 var configFile = JSON.parse(fs.readFileSync("./astra.config.json", "utf-8"));
-
-
 
 var variables = "";
 if(configFile.using.variables){
     variables = JSON.parse(fs.readFileSync("./variables.json","utf8"));
 } 
+var base = "";
+if(configFile.using.base){
+    base = fs.readFileSync("./base.css","utf-8");
+}
 
 var outputData = "";
-function closeBrackets(){
-    outputData += "}";
-}
+
 
 //CSS variables
 outputData += ":root{\n";
 for(let key in variables){ 
     if(configFile.included.variables[key]){
-        console.log("There are modifications")
         variables[key] = configFile.included.variables[key];
     }
     outputData += `    --${key}: ${variables[key]}; \n`
@@ -25,11 +27,10 @@ for(let key in variables){
 for(let key in configFile.extra.variables){
     outputData += `    --${key}: ${configFile.extra.variables[key]}; \n`
 }
-closeBrackets();
+outputData += "}";
+//Base
+outputData += base;
 
-
-
-
-var output = fs.writeFileSync("./output/output.css", outputData ,function(err){ if(err) throw err;});
-
-console.log("Creating CSS file...");
+fs.writeFileSync("./output/output.css", outputData ,function(err){ if(err) throw err;});
+var endTime = performance.now()
+console.log(`Finished in ${(endTime - startTime).toFixed(0)}ms`)
