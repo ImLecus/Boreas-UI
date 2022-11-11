ca346c4b6a5f71df607fc316ea248d2ef59939af
ImLecus <marcosgf2005@gmail.com>
2022-11-11 21:10:53 +0100

    Parsing html files

--- astra/astra.js
+++ astra/astra.js
@@ -1,7 +1,8 @@
 console.log("Compiling...");
 var startTime = performance.now()
 var fs = require("node:fs");
-
+var path = require('path');
+const parse = require('node-html-parser').parse;
 var configFile = JSON.parse(fs.readFileSync("./astra.config.json", "utf-8"));
 
 var variables = "";
@@ -14,7 +15,10 @@ if(configFile.using.base){
 }
 
 var outputData = "";
-
+var outputDir = process.argv[2];
+if(process.argv[2] && process.argv[2].endsWith(".css")){
+    outputDir = process.argv[2];
+}
 
 //CSS variables
 outputData += ":root{\n";
@@ -29,8 +33,22 @@ for(let key in configFile.extra.variables){
 }
 outputData += "}";
 //Base
+fs.readdir("./",function(err,data){
+    if(err) throw err;
+    let input = data.filter(d => d.endsWith(".html"));
+    input.forEach(file => {
+        let content = fs.readFileSync(file,"utf-8");
+        let document = parse(content);
+        document.querySelectorAll("body *").forEach(element => {
+            console.log(element.toString())
+        })
+
+
+    });
+});
+
 outputData += base;
 
-fs.writeFileSync("./output/output.css", outputData ,function(err){ if(err) throw err;});
+fs.writeFileSync(outputDir, outputData ,function(err){ if(err) throw err;});
 var endTime = performance.now()
-console.log(`Finished in ${(endTime - startTime).toFixed(0)}ms`)
+console.log(`Finished in ${(endTime - startTime).toFixed(0)}ms`);
